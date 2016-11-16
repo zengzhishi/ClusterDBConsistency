b5e6b5b8d3c0abb6432b445fed14d33c9b0275ad
zengzhishi <zudaojun@outlook.com>
2016-11-16 18:28:43 +0800

    init

---
+++ .idea/fileTemplates/includes/File Header.java
@@ -0,0 +1,9 @@
+#set ($SITE = "http://uestc.xyz")
+#set ($GROUP = "lug")
+/**
+ * ${DESCRIPTION}
+ * ${SITE}
+ * @author ${USER}
+ * @group ${GROUP}
+ * @create ${DATE}
+ */
---
+++ .idea/fileTemplates/internal/Class.java
@@ -0,0 +1,12 @@
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
+#set ($SITE = "http://uestc.xyz")
+#set ($GROUP = "lug")
+/**
+ * ${DESCRIPTION}
+ * ${SITE}
+ * @author ${USER}
+ * @group ${GROUP}
+ * @create ${DATE}
+ */
+public class ${NAME} {
+}
